ba553135a304f2b46158c45a4d9c128ec6c449c3
VinciDaKinG <kamil.pajak75@wp.pl>
2019-05-28 01:08:51 +0200

    commit

---
+++ ClientApp/src/App.js
@@ -0,0 +1,291 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+class App extends Component {
+  state = {
+    laws: [],
+    newLawsData: {
+      LawText: "",
+      DateAdd: "",
+      DateEnd: ""
+    },
+    editLawsData: {
+      id: "",
+      LawText: "",
+      DateAdd: "",
+      DateEnd: ""
+    },
+    newLawsModal: false,
+    editLawsModal: false
+  };
+
+  // componetWillMount() {
+  //   axios.get("http://localhost:57548/laws/").then(response => {
+  //     this.setState({
+  //       laws: response.data
+  //     });
+  //   });
+  // }
+
+  componentWillMount() {
+    this._refreshLaw();
+  }
+
+  toggleNewLawsModel() {
+    this.setState({
+      newLawsModal: !this.state.newLawsModal
+    });
+  }
+
+  toggleEditLawsModel() {
+    this.setState({
+      editLawsModal: !this.state.editLawsModal
+    });
+  }
+
+  addLaw() {
+    axios
+      .post("http://localhost:57548/api/laws/", this.state.newLawsData)
+      .then(response => {
+        let { laws } = this.state;
+
+        laws.push(response.data);
+
+        this.setState({
+          laws,
+          newLawsModal: false,
+          newLawsData: {
+            LawText: "",
+            DateAdd: "",
+            DateEnd: ""
+          }
+        });
+      });
+  }
+
+  updateLaws() {
+    let { LawText, DateAdd, DateEnd } = this.state.editLawsData;
+    axios
+      .put("http://localhost:57548/api/laws/" + this.state.editLawsData.id, {
+        LawText,
+        DateAdd,
+        DateEnd
+      })
+      .then(response => {
+        this._refreshLaw();
+      });
+  }
+
+  editLaws(id, LawText, DateAdd, DateEnd) {
+    this.setState({
+      editLawsData: { id, LawText, DateAdd, DateEnd },
+      editLawsModel: !this.state.editLawsModal
+    });
+  }
+
+  deleteLaw(id) {
+    axios.delete("http://localhost:57548/api/laws/" + id).then(response => {
+      this._refreshLaw();
+    });
+  }
+
+  _refreshLaw() {
+    // axios.get(`http://localhost:57548/laws/`).then(res => {
+    //   const laws = res.data;
+    //   this.setState({ laws });
+    // });
+    axios.get("http://localhost:57548/api/laws/").then(response => {
+      this.setState({
+        laws: response.data
+      });
+    });
+  }
+
+  render() {
+    let laws = this.state.laws.map(law => {
+      return (
+        <tr key={law.id}>
+          <td>{law.LawText}</td>
+          <td>{law.DateAdd}</td>
+          <td>{law.DateEnd}</td>
+          <td>
+            <Button
+              color="success"
+              size="sm"
+              className="mr-2"
+              onClick={this.editLaws.bind(
+                this,
+                law.id,
+                law.LawText,
+                law.DateAdd,
+                law.DateEnd
+              )}
+            >
+              Edit
+            </Button>
+            <Button
+              color="danger"
+              size="sm"
+              onClick={this.deleteLaw.bind(this, law.id)}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+    return (
+      <div className="App container">
+        <h1>Dodanie Uchwały</h1>
+        <Button
+          className="my-3"
+          color="primary"
+          onClick={this.toggleNewLawsModel.bind(this)}
+        >
+          Dodanie Uchwały
+        </Button>
+        <Modal
+          isOpen={this.state.newLawsModal}
+          toggle={this.toggleNewLawsModel.bind(this)}
+        >
+          <ModalHeader toggle={this.toggleNewLawsModel.bind(this)}>
+            Dodanie nowej uchwaly
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="LawText">Tresc</Label>
+              <Input
+                id="LawText"
+                placeholder="Podaj Tresc"
+                value={this.state.newLawsData.LawText}
+                onChange={e => {
+                  let { newLawsData } = this.state;
+                  newLawsData.LawText = e.target.value;
+                  this.setState({ newLawsData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="DateAdd">Data Dodania</Label>
+              <Input
+                id="DateAdd"
+                placeholder="Podaj Date"
+                value={this.state.newLawsData.DateAdd}
+                onChange={e => {
+                  let { newLawsData } = this.state;
+                  newLawsData.DateAdd = e.target.value;
+                  this.setState({ newLawsData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="DateEnd">Data Konca</Label>
+              <Input
+                id="DateEnd"
+                placeholder="Podaj Date"
+                value={this.state.newLawsData.DateEnd}
+                onChange={e => {
+                  let { newLawsData } = this.state;
+                  newLawsData.DateEnd = e.target.value;
+                  this.setState({ newLawsData });
+                }}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.addLaw.bind(this)}>
+              Dodaj
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleNewLawsModel.bind(this)}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <Modal
+          isOpen={this.state.editLawsModal}
+          toggle={this.toggleEditLawsModel.bind(this)}
+        >
+          <ModalHeader toggle={this.toggleEditLawsModel.bind(this)}>
+            Edytowanie Uchwały
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="LawText">Tresc</Label>
+              <Input
+                id="LawText"
+                value={this.state.editLawsData.LawText}
+                onChange={e => {
+                  let { editLawsData } = this.state;
+                  editLawsData.LawText = e.target.value;
+                  this.setState({ editLawsData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="DateAdd">Data</Label>
+              <Input
+                id="DateAdd"
+                value={this.state.editLawsData.DateAdd}
+                onChange={e => {
+                  let { editLawsData } = this.state;
+                  editLawsData.DateAdd = e.target.value;
+                  this.setState({ editLawsData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="DateEnd">Data</Label>
+              <Input
+                id="DateEnd"
+                value={this.state.editLawsData.DateEnd}
+                onChange={e => {
+                  let { editLawsData } = this.state;
+                  editLawsData.DateEnd = e.target.value;
+                  this.setState({ editLawsData });
+                }}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.updateLaws.bind(this)}>
+              Edycja
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleEditLawsModel.bind(this)}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Tresc</th>
+              <th>Data Dodania</th>
+              <th>Data Konca</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{laws}</tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ ClientApp/src/componets/LawPage.js
@@ -0,0 +1,323 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {
+  Table,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  FormGroup,
+  Input,
+  Label
+} from "reactstrap";
+
+class App extends Component {
+  state = {
+    users: [],
+    newUserData: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: ""
+    },
+    editUserData: {
+      id: "",
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: ""
+    },
+    newUserModal: false,
+    editUserModal: fals
+  };
+
+  // componetWillMount() {
+  //   axios.get("http://localhost:57548/users/").then(response => {
+  //     this.setState({
+  //       users: response.data
+  //     });
+  //   });
+  // }
+
+  componentWillMount() {
+    this._refreshUser();
+  }
+
+  toggleNewUserModel() {
+    this.setState({
+      newUserModal: !this.state.newUserModal
+    });
+  }
+
+  toggleEditUserModel() {
+    this.setState({
+      editUserModal: !this.state.editUserModal
+    });
+  }
+
+  addUser() {
+    axios
+      .post("http://localhost:57548/users/registred", this.state.newUserData)
+      .then(response => {
+        let { users } = this.state;
+
+        users.push(response.data);
+
+        this.setState({
+          users,
+          newUserModal: false,
+          newUserData: {
+            firstName: "",
+            lastName: "",
+            username: "",
+            password: ""
+          }
+        });
+      });
+  }
+
+  updateUser() {
+    let { firstName, lastName, username, password } = this.state.editUserData;
+    axios
+      .put("http://localhost:57548/users/" + this.state.editUserData.id, {
+        firstName,
+        lastName,
+        username,
+        password
+      })
+      .then(response => {
+        this._refreshUser();
+      });
+  }
+
+  editUser(id, firstName, lastName, username, password) {
+    this.setState({
+      editUserData: { id, firstName, lastName, username, password },
+      editUserModel: !this.state.editUserModal
+    });
+  }
+
+  deleteUser(id) {
+    axios.delete("http://localhost:57548/users" + id).then(response => {
+      this._refreshUser();
+    });
+  }
+
+  _refreshUser() {
+    // axios.get(`http://localhost:57548/users/`).then(res => {
+    //   const users = res.data;
+    //   this.setState({ users });
+    // });
+    axios.get("http://localhost:57548/users/").then(response => {
+      this.setState({
+        users: response.data
+      });
+    });
+  }
+
+  render() {
+    let users = this.state.users.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.firstName}</td>
+          <td>{user.lastName}</td>
+          <td>{user.username}</td>
+          <td>
+            <Button
+              color="success"
+              size="sm"
+              className="mr-2"
+              onClick={this.editUser.bind(
+                this,
+                user.id,
+                user.firstName,
+                user.lastName,
+                user.username,
+                user.password
+              )}
+            >
+              Edit
+            </Button>
+            <Button
+              color="danger"
+              size="sm"
+              onClick={this.deleteUser.bind(this, user.id)}
+            >
+              Delete
+            </Button>
+          </td>
+        </tr>
+      );
+    });
+    return (
+      <div className="App container">
+        <h1>Dodanie Uzytkownika</h1>
+        <Button
+          className="my-3"
+          color="primary"
+          onClick={this.toggleNewUserModel.bind(this)}
+        >
+          Dodanie Uzytkownika
+        </Button>
+        <Modal
+          isOpen={this.state.newUserModal}
+          toggle={this.toggleNewUserModel.bind(this)}
+        >
+          <ModalHeader toggle={this.toggleNewUserModel.bind(this)}>
+            Dodanie nowego użytkownika
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="firstName">Imie</Label>
+              <Input
+                id="firstName"
+                placeholder="Podaj Imie"
+                value={this.state.newUserData.firstName}
+                onChange={e => {
+                  let { newUserData } = this.state;
+                  newUserData.firstName = e.target.value;
+                  this.setState({ newUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastname">Nazwisko</Label>
+              <Input
+                id="lastname"
+                placeholder="Podaj Nazwisko"
+                value={this.state.newUserData.lastName}
+                onChange={e => {
+                  let { newUserData } = this.state;
+                  newUserData.lastName = e.target.value;
+                  this.setState({ newUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="username">Email</Label>
+              <Input
+                id="username"
+                placeholder="Podaj Emiala"
+                value={this.state.newUserData.username}
+                onChange={e => {
+                  let { newUserData } = this.state;
+                  newUserData.username = e.target.value;
+                  this.setState({ newUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                id="firsname"
+                placeholder="Podaj Hasło"
+                value={this.state.newUserData.password}
+                onChange={e => {
+                  let { newUserData } = this.state;
+                  newUserData.password = e.target.value;
+                  this.setState({ newUserData });
+                }}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.addUser.bind(this)}>
+              Dodaj
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleNewUserModel.bind(this)}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+        <Modal
+          isOpen={this.state.editUserModal}
+          toggle={this.toggleEditUserModel.bind(this)}
+        >
+          <ModalHeader toggle={this.toggleEditUserModel.bind(this)}>
+            Edytowanie Uzytkownika
+          </ModalHeader>
+          <ModalBody>
+            <FormGroup>
+              <Label for="firstName">Imie</Label>
+              <Input
+                id="firstName"
+                value={this.state.editUserData.firstName}
+                onChange={e => {
+                  let { editUserData } = this.state;
+                  editUserData.firstName = e.target.value;
+                  this.setState({ editUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="lastname">Nazwisko</Label>
+              <Input
+                id="lastname"
+                value={this.state.editUserData.lastName}
+                onChange={e => {
+                  let { editUserData } = this.state;
+                  editUserData.lastName = e.target.value;
+                  this.setState({ editUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="username">Email</Label>
+              <Input
+                id="username"
+                value={this.state.editUserData.username}
+                onChange={e => {
+                  let { editUserData } = this.state;
+                  editUserData.username = e.target.value;
+                  this.setState({ editUserData });
+                }}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                id="firsname"
+                value={this.state.editUserData.password}
+                onChange={e => {
+                  let { editUserData } = this.state;
+                  editUserData.password = e.target.value;
+                  this.setState({ editUserData });
+                }}
+              />
+            </FormGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.updateUser.bind(this)}>
+              Edycja
+            </Button>{" "}
+            <Button
+              color="secondary"
+              onClick={this.toggleEditUserModel.bind(this)}
+            >
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
+
+        <Table>
+          <thead>
+            <tr>
+              <th>Imie</th>
+              <th>Nazwisko</th>
+              <th>Login</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{users}</tbody>
+        </Table>
+      </div>
+    );
+  }
+}
+export default App;
