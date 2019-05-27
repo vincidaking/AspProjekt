7b54390d2689feadaed87e26fae5fe03bbaa2df2
VinciDaKinG <kamil.pajak75@wp.pl>
2019-05-28 01:09:10 +0200

    commit

---
+++ Migrations/20190526220716_init2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Apka2.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "User",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateTable(
+                name: "Laws",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LawText = table.Column<string>(nullable: true),
+                    DateAdd = table.Column<DateTime>(nullable: false),
+                    DateEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laws", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Laws");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "User",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
